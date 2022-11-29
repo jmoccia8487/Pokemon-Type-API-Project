6832c5edbdec29d117c605625e074f31773b6de2
jmoccia8487 <117880452+jmoccia8487@users.noreply.github.com>
2022-11-29 15:41:10 -0500

    Update FXMLPokeTypeAppController.java

--- FXMLPokeTypeAppController.java
+++ FXMLPokeTypeAppController.java
@@ -1,9 +1,7 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
 import java.util.ResourceBundle;
 import java.util.Date;
 import java.util.prefs.Preferences;
